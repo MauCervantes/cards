6688d716f91dc0e626dfa12a12372eca2f6cbfc3
MauCervantes <maucarranco016@gmail.com>
2023-11-19 19:13:55 -0600

    first commit

---
+++ react-cards/src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import './Card.css';
+
+const Card = ({lang, img, fcolor, scolor}) => {
+
+    return (
+        <div
+        className='card'
+        style={{background: `linear-gradient(to left, ${fcolor}, ${scolor})`}}>
+            <img src={img} alt="lang.svg" />
+            <h3>{lang}</h3>
+        </div>
+    );
+};
+//Definimos los porptypes de Card
+Card.PropTypes = {
+    lang: PropTypes.string,
+    img: PropTypes.string,
+    fcolor: PropTypes.string,
+    scolor: PropTypes.string
+};
+export default Card;
